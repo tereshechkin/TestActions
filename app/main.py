ddd06e87abc531559ad662dd0c8404138a0c1a39
Mikhail Tereshechkin <tereshechkin@genix.pro>
2026-03-30 17:42:00 +0300

    Добавлены тестовые endpoint

--- app/main.py
+++ app/main.py
@@ -8,3 +8,13 @@ app = FastAPI(title="Test Time Backend")
 @app.get("/time")
 def get_server_time() -> dict[str, str]:
     return {"server_time": datetime.now(timezone.utc).isoformat()}
+
+
+@app.get("/date")
+def get_server_date() -> dict[str, str]:
+    return {"server_date": datetime.now().date().isoformat()}
+
+
+@app.get("/date/utc")
+def get_server_date_utc() -> dict[str, str]:
+    return {"server_date_utc": datetime.now(timezone.utc).date().isoformat()}
